f763de1d701b867ab5a1f8c4d1e5d936cbbce22f
Lucas Cottet Pastor <161661488+lucascottetpastor@users.noreply.github.com>
2024-10-15 12:45:46 -0300

    Update extrair.py

--- extrair.py
+++ extrair.py
@@ -6,7 +6,7 @@ from fpdf import FPDF
 import os
 import stat
 
-uploaded_file = r"C:\Users\lucas\OneDrive\Área de Trabalho\Planilha_modelo_dash.xlsx"
+uploaded_file = "Planilha_modelo_dash.xlsx"
 
 def garantir_permissoes_pasta(pasta):
     if not os.path.exists(pasta):
